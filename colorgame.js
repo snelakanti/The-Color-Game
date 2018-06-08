fc652b783c300f7dd6d6ef8498fdbd0051b46e08
snelakanti <vnelakanti@gmail.com>
2018-06-07 22:24:57 -0400

    modify h1 styling

--- colorgame.js
+++ colorgame.js
@@ -57,7 +57,7 @@ reset.addEventListener("click", function(){
        squares[i].style.backgroundColor = colors[i];
    }
 
-   h1.style.backgroundColor = "#232323";
+   h1.style.backgroundColor = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
